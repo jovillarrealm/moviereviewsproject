615c0b3e0a5a623897bc35ebdc35eec52c3ce0ff
Jorge A. Villarreal <88699002+jovillarrealm@users.noreply.github.com>
2023-08-03 11:02:51 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     return render(req, "home.html", 
-                  {"name": "GREAGGH LIM"})
+                  {"name": "Jorge A. Villarreal Márquez"})
