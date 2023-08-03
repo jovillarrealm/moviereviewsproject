72f1d983e2794291a042f3f50c528d24d9dc87a4
Jorge A. Villarreal <88699002+jovillarrealm@users.noreply.github.com>
2023-08-03 11:36:43 -0500

    ultimo commit

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 # Create your views here.
+dic = {"name": "Jorge A. Villarreal Márquez"}
 def home(req):
     return render(req, "home.html", 
-                  {"name": "Jorge A. Villarreal Márquez"})
+                  dic)
+def about(req):
+    return render(req, "about.html", 
+                  dic)
